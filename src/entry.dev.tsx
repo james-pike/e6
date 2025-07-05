ea2b483792b6297bf08fcfd0fd0ea8e668a0d4c0
Your Name <you@example.com>
2025-07-05 15:42:04 -0400

    Initialize Vercel Edge Adapter

--- src/entry.dev.tsx
+++ src/entry.dev.tsx
@@ -7,7 +7,7 @@
  * - No portion of the application is pre-rendered on the server.
  * - All of the application is running eagerly in the browser.
  * - More code is transferred to the browser than in SSR mode.
- * - Optimizer/Serialization/Deserialization code is not exercised!
+ * - Optimizer/Serialization/Deserialization code is not exercised!s
  */
 import { render, type RenderOptions } from '@builder.io/qwik';
 import Root from './root';
