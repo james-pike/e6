6e96787c6fea6bbf99dfa38840344193c9a7ec73
your name <you@exammple.com>
2025-08-22 15:27:27 -0400

    commit

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,33 +1,8 @@
 import { QwikAuth$ } from "@auth/qwik";
-import Twitter from "@auth/qwik/providers/twitter";
-import Credentials from "@auth/qwik/providers/credentials";
+import GitHub from "@auth/qwik/providers/github";
 
 export const { onRequest, useSession, useSignIn, useSignOut } = QwikAuth$(
   () => ({
-    providers: [
-      Twitter,
-      Credentials({
-        async authorize(credentials) {
-          // Set your admin username and password here
-          const adminUsername = "admin";
-          const adminPassword = "Earthen";
-          if (
-            credentials.username === adminUsername &&
-            credentials.password === adminPassword
-          ) {
-            return {
-              id: "admin",
-              name: "Admin User",
-              email: "admin@example.com",
-            };
-          }
-          return null;
-        },
-        credentials: {
-          username: { label: "Username", type: "text" },
-          password: { label: "Password", type: "password" },
-        },
-      }),
-    ],
+    providers: [GitHub],
   }),
 );
